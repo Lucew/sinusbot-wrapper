27e5595c7d57f0146fb0a226b60ae9afef5d3f5a
Lucas <weber-lucas@web.de>
2025-07-06 16:58:00 +0200

    update the path to the file

--- yt-dlp-files/shell-yt-dlp-wrapper.sh
+++ yt-dlp-files/shell-yt-dlp-wrapper.sh
@@ -3,4 +3,4 @@
 # Ensure the script is executable: chmod +x run-yt-dlp-wrapper.sh
 
 # Run the Python script with all passed arguments
-python yt-dlp-wrapper.py "$@"
+python /opt/sinusbot/yt-dlp-wrapper.py "$@"
