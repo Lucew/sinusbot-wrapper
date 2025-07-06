975980e9d00d1a8389336fb53beb6845974b9f40
Lucas <weber-lucas@web.de>
2025-07-06 18:03:43 +0200

    refine script outpuh

--- yt-dlp-files/shell-yt-dlp-wrapper.sh
+++ yt-dlp-files/shell-yt-dlp-wrapper.sh
@@ -3,4 +3,4 @@
 # Ensure the script is executable: chmod +x run-yt-dlp-wrapper.sh
 
 # Run the Python script with all passed arguments
-python /opt/sinusbot/yt-dlp-files/yt-dlp-wrapper.py "$@"d
+python /opt/sinusbot/yt-dlp-files/yt-dlp-wrapper.py "$@" 2>&1
