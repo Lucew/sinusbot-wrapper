273f00f652c40fbc1ff9c6899dc3e19a3168e74b
Lucas <weber-lucas@web.de>
2025-08-13 22:40:43 +0200

    change output format to fix error

--- yt-dlp-wrapper.py
+++ yt-dlp-wrapper.py
@@ -28,9 +28,28 @@ if not os.path.isfile(cookie_path):
     logging.error(error_msg)
     raise FileNotFoundError(error_msg)
 
+
+"""
+-t mp3                          -f 'ba[acodec^=mp3]/ba/b' -x --audio-format
+                                mp3
+
+-t aac                          -f
+                                'ba[acodec^=aac]/ba[acodec^=mp4a.40.]/ba/b'
+                                -x --audio-format aac
+
+-t mp4                          --merge-output-format mp4 --remux-video mp4
+                                -S vcodec:h264,lang,quality,res,fps,hdr:12,a
+                                codec:aac
+
+-t mkv                          --merge-output-format mkv --remux-video mkv
+
+-t sleep                        --sleep-subtitles 5 --sleep-requests 0.75
+                                --sleep-interval 10 --max-sleep-interval 20
+"""
+
 # get the arguments the file is run with
 args = sys.argv
-command = [r"/opt/sinusbot/yt-dlp", "--cookies", cookie_path, *args[1:]]
+command = [r"/opt/sinusbot/yt-dlp", "--cookies", cookie_path, "-t", "aac", *args[1:]]
 
 # run the yt-dlp command but add the cookies
 try:
